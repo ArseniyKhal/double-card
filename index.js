a0aaa56cdf96d05bbb9e193ec65a65785a2f3b53
Arseniy <asmn@mail.ru>
2023-06-30 12:02:09 +0500

    module - absolute

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ export let gameState = {
 
 renderApp();
 
+// выбор сложности игры
+const buttonsDifficultyLevel = document.querySelectorAll(".difficulty-level__element");
+for (const buttonDifLevel of buttonsDifficultyLevel) {
+	buttonDifLevel.addEventListener('click', () => {
+		gameState.difficultyLevel = buttonDifLevel.dataset.level;
+		renderApp();
+	});
+}
+
+// начало новой игры
+// const buttonNewGame = document.getElementById("newGame");
+// console.log(buttonNewGame);
+// buttonNewGame.addEventListener('click', () => {
+// 	console.log("2");
+	// gameState.difficultyLevel = 0;
+	// renderApp();
+// })
 
--- renderApp.js
+++ renderApp.js
@@ -1,37 +1,7 @@
 import { gameState } from "./index.js";
 
-
-
-
-
-export const renderApp = () => {
-	const appEl = document.getElementById("app");
-	if (gameState.difficultyLevel === 0) {
-		const appHtml =
-			`<section class="modal">
-				<div class="modal__container">
-					<div class="modal__body difficulty-level">
-						<div class="modal__content">
-							<h2 class="difficulty-level__title">Выбери <br> сложность</h2>
-							<div class="difficulty-level__row">
-								<div class="difficulty-level__element">1</div>
-								<div class="difficulty-level__element">2</div>
-								<div class="difficulty-level__element">3</div>
-							</div>
-						</div>
-						<div class="modal__button">
-							<button class="difficulty-level__button button">Старт</button>
-						</div>
-					</div>
-				</div>
-			</section>`
-
-		appEl.innerHTML = appHtml;
-
-	} else {
-		const appHtml =
-			`
-			<header class="header">
+const headerHtml =
+	`<header class="header">
 				<div class="container">
 					<div class="header__body">
 						<div class="header__timer timer">
@@ -41,18 +11,63 @@ export const renderApp = () => {
 							</div>
 							<div class="timer__volume">00.00</div>
 						</div>
-						<button class="header__button button">Начать заново</button>
+						<button class="header__button button" id="newGame">Начать заново</button>
 					</div>
 				</div>
 			</header>
-			<section class="card-field">
-				<div class="container">
-					<div class="card-field__body">
-						поле с картами
-					</div>
+	`;
+
+const sectionModulHtml =
+	`<section class="modal">
+	<div class="modal__container">
+		<div class="modal__body difficulty-level">
+			<div class="modal__content">
+				<h2 class="difficulty-level__title">Выбери <br> сложность</h2>
+				<div class="difficulty-level__row">
+					<div class="difficulty-level__element" data-level="1">1</div>
+					<div class="difficulty-level__element" data-level="2">2</div>
+					<div class="difficulty-level__element" data-level="3">3</div>
 				</div>
-			</section>
+			</div>
+			<div class="modal__button">
+				<button class="difficulty-level__button button">Старт</button>
+			</div>
+		</div>
+	</div>
+</section>
+	`;
+
+const sectionCardFieldHtml =
+	`<section class="card-field">
+	<div class="container">
+		<div class="card-field__body">
+			поле с картами
+		</div>
+	</div>
+</section>
+	`;
+
+
+
+
+
+export const renderApp = () => {
+	const appEl = document.getElementById("app");
+	if (gameState.difficultyLevel === 0) {
+		const appHtml =
 			`
+			${sectionModulHtml}
+			`
+		appEl.innerHTML = appHtml;
+
+	} else {
+
+		const appHtml =
+			`
+		${headerHtml}
+		${sectionModulHtml}
+		${sectionCardFieldHtml}
+		`
 
 		appEl.innerHTML = appHtml;
 	}
@@ -61,20 +76,5 @@ export const renderApp = () => {
 
 
 
-const headerHtml =
-	`<header class="header">
-		<div class="container">
-			<div class="header__body">
-				<div class="header__timer timer">
-					<div class="timer__text">
-						<div class="timer__min">min</div>
-						<div class="timer__sek">sek</div>
-					</div>
-					<div class="timer__volume">00.00</div>
-				</div>
-				<button class="header__button button">Начать заново</button>
-			</div>
-		</div>
-	</header>`
 
 
